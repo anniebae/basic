c4d4e00110a8184e0ed49af5e8cf1a9cfd0cc63e
anniebae <bae.annie@gmail.com>
2014-12-18 10:50:04 -0500

    styling...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-# ****** AFTER ANDREW *******
-
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates :username, uniqueness: true
+
 
   attr_accessor :instagram_account_id, :instagram_profile_picture
 
