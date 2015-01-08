2f310a2ef8a65c45092a8d273f51e9fd7346c307
anniebae <bae.annie@gmail.com>
2015-01-08 17:10:26 -0500

    Sean trying to fix thingz

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,8 @@
 //= require bootstrap.min
 //= require underscore
 //= require backbone
-//= require backbone_app/models/User
-//= require backbone_app/collections/user_collection
-//= require backbone_app/views/UserTableView
-//= require backbone_app/views/user_profile_view
-//= require backbone_app/routers/router
+//= require_tree ./backbone_app/routers
+//= require_tree ./backbone_app/models
+//= require_tree ./backbone_app/collections
+//= require_tree ./backbone_app/views
 //= require main
