aac8db2287cd3440bf8c5602aea656ddad0acd16
anniebae <bae.annie@gmail.com>
2015-01-08 19:16:37 -0500

    finding things in git log

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,9 @@
 //= require bootstrap.min
 //= require underscore
 //= require backbone
-//= require backbone_app/models/User
 //= require backbone_app/collections/user_collection
+//= require backbone_app/models/User
+//= require backbone_app/routers/router
 //= require backbone_app/views/UserTableView
 //= require backbone_app/views/user_profile_view
-//= require backbone_app/routers/router
 //= require main
