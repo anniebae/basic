f954796d46aa9a5e5010562bacb8aae99728f855
anniebae <bae.annie@gmail.com>
2015-01-14 16:59:40 -0500

    testing userCollection

--- app/assets/javascripts/backbone_app/collections/user_collection.js
+++ app/assets/javascripts/backbone_app/collections/user_collection.js
@@ -1,6 +1,7 @@
 console.log("UserCollection yay");
 
 var UserCollection = Backbone.Collection.extend({
-	model: User,
-	url: '/users'
+  model: User,
+  url: '/users'
+  console.log("UserCollection rendered");
 })
